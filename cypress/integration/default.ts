40fa5a3b19b06faba88d56e913be98c5b8421b7b
Hewon Jeong <hewon.jng@gmail.com>
2019-12-16 00:47:01 +0900

    test: test overview page landing

--- cypress/integration/default.ts
+++ cypress/integration/default.ts
@@ -1,6 +1,16 @@
 describe('Default', () => {
-  it('Signin to SendBird Dashboard', () => {
+  it('rnders applications when signin', () => {
     cy.login()
-    cy.get('tbody tr').should('have.length.greaterThan', 1)
+    cy.get('tbody tr.etQUDs').should('have.length.greaterThan', 1)
+  })
+
+  it('renders overview page when click application', () => {
+    cy.login()
+
+    cy.get('tbody tr.etQUDs td')
+      .first()
+      .click({ force: true })
+
+    cy.url().should('include', '/overview')
   })
 })
